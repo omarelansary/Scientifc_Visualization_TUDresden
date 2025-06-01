4f0240f5a4de97973773872f84b7c15e30bcf6ae
Georg Kunze <georgm.kunze@gmail.com>
2025-06-01 17:58:14 +0200

    implement bonus task 2.3.a

--- 2-particle/particle.cxx
+++ 2-particle/particle.cxx
@@ -1,47 +1,46 @@
-// This source code is property of the Computer Graphics and Visualization chair of the
-// TU Dresden. Do not distribute! 
-// Copyright (C) CGV TU Dresden - All Rights Reserved
+// This source code is property of the Computer Graphics and Visualization
+// chair of the TU Dresden. Do not distribute! Copyright (C) CGV TU Dresden -
+// All Rights Reserved
 
-#include <fstream>
-#include <sstream>
-
-#include <cgv/base/node.h> // this should be first include to avoid warning under VS
+#include <cgv/base/node.h>  // this should be first include to avoid warning under VS
 #include <cgv/base/register.h>
 #include <cgv/data/data_view.h>
-#include <cgv/math/ftransform.h>
-#include <cgv/media/mesh/simple_mesh.h>
-#include <cgv_reflect_types/media/color.h>
-#include <cgv_gl/gl/gl.h> // includes also GL/gl.h
-#include <cgv/render/drawable.h>
-#include <cgv/render/shader_program.h>
-#include <cgv/render/attribute_array_binding.h>
-#include <cgv/render/vertex_buffer.h>
-#include <cgv/gui/provider.h>
 #include <cgv/gui/event_handler.h>
 #include <cgv/gui/key_event.h>
+#include <cgv/gui/provider.h>
+#include <cgv/math/ftransform.h>
+#include <cgv/media/mesh/simple_mesh.h>
+#include <cgv/render/attribute_array_binding.h>
+#include <cgv/render/drawable.h>
+#include <cgv/render/shader_program.h>
+#include <cgv/render/vertex_buffer.h>
+#include <cgv_gl/gl/gl.h>  // includes also GL/gl.h
+#include <cgv_reflect_types/media/color.h>
+
+#include <fstream>
 #include <random>
+#include <sstream>
 
 using namespace cgv::render;
 using namespace cgv::math;
 
-class particle :
-	public cgv::base::node,
-	public cgv::render::multi_pass_drawable,
-	public cgv::gui::event_handler,
-	public cgv::gui::provider
-{
-private:
-
-protected:
+class particle : public cgv::base::node,
+				 public cgv::render::multi_pass_drawable,
+				 public cgv::gui::event_handler,
+				 public cgv::gui::provider {
+   private:
+   protected:
 	/// file name of rendered scene mesh
 	std::string file_name;
 
 	/// data storage for molecule
-	std::vector<cgv::vec3> positions;     // (x,y,z)-coordinate of atom center
-	std::vector<cgv::vec4> colors;        // colors for each atom
-	std::vector<GLuint> connections;                            // connections stored as indices
+	std::vector<cgv::vec3> positions;  // (x,y,z)-coordinate of atom center
+	std::vector<cgv::vec4> colors;     // colors for each atom
+	std::vector<GLuint> connections;   // connections stored as indices
+	std::vector<int>
+		num_connections;  // number of connections for each atom pair
 
-	cgv::render::view* view_ptr;
+	cgv::render::view *view_ptr;
 
 	/// rendering
 	cgv::media::illum::surface_material material_mol, material_plane;
@@ -51,6 +50,7 @@ protected:
 	cgv::render::vertex_buffer vb_col;
 	cgv::render::attribute_array_binding vertex_array_spheres;
 	cgv::render::attribute_array_binding vertex_array_cylinder;
+	GLuint connection_ssbo;
 	float radius;
 
 	bool show_spheres;
@@ -58,10 +58,9 @@ protected:
 
 	bool show_plane;
 	float plane_size, plane_offset;
-public:
-	particle() :
-		node("particle")
-	{
+
+   public:
+	particle() : node("particle") {
 		file_name = "./data/molecule_C34H40F2N4O4.sdf";
 		radius = 0.25f;
 		show_spheres = true;
@@ -70,40 +69,30 @@ public:
 		plane_offset = 0;
 		show_plane = false;
 
-		material_mol.set_brdf_type(
-			(cgv::media::illum::BrdfType)(cgv::media::illum::BT_LAMBERTIAN | cgv::media::illum::BT_PHONG)
-		);
-		material_mol.ref_specular_reflectance() = { .0625f, .0625f, .0625f };
+		material_mol.set_brdf_type((cgv::media::illum::BrdfType)(
+			cgv::media::illum::BT_LAMBERTIAN | cgv::media::illum::BT_PHONG));
+		material_mol.ref_specular_reflectance() = {.0625f, .0625f, .0625f};
 		material_mol.ref_roughness() = .0625f;
 		material_plane = material_mol;
-		material_plane.ref_diffuse_reflectance() = { .389f, .354f, .0084f };
+		material_plane.ref_diffuse_reflectance() = {.389f, .354f, .0084f};
 		material_plane.ref_roughness() = .015625f;
 	}
 
+	std::string get_type_name() const { return "particle"; }
 
-	std::string get_type_name() const
-	{
-		return "particle";
+	/// makes data member of this class available as named properties (which
+	/// can be set in a config file)
+	bool self_reflect(cgv::reflect::reflection_handler &rh) {
+		return rh.reflect_member("file_name", file_name);
 	}
-
-
-	/// makes data member of this class available as named properties (which can be set in a config file)
-	bool self_reflect(cgv::reflect::reflection_handler& rh)
-	{
-		return
-			rh.reflect_member("file_name", file_name);
-	}
-
 
 	/// callback for all changed UI elements
-	void on_set(void* member_ptr)
-	{
+	void on_set(void *member_ptr) {
 		update_member(member_ptr);
 		post_redraw();
 	}
 
-	bool read_sdf_file(std::string file_name)
-	{
+	bool read_sdf_file(std::string file_name) {
 		std::ifstream file;
 		file.open(file_name.c_str());
 		if (!file)
@@ -128,15 +117,12 @@ public:
 		iiss >> number_connections;
 
 		int iteration = 0;
-		while (std::getline(file, line))
-		{
+		while (std::getline(file, line)) {
 			if (line.length() == 0 || line[0] == '#')
 				continue;
-			else
-			{
+			else {
 				// first read atoms
-				if (iteration < number_atoms)
-				{
+				if (iteration < number_atoms) {
 					std::stringstream ss(line);
 					// center coordinates
 					ss >> position.x() >> position.y() >> position.z();
@@ -160,42 +146,45 @@ public:
 						colors.push_back(cgv::vec4(0.2f, 0.2f, 0.2f, 1.0f));
 				}
 				// read connections
-				else
-				{
+				else {
 					std::istringstream iss(line);
-					int from, to;
-					iss >> from >> to;
+					int from, to, n;
+					iss >> from >> to >> n;
 
-					// store start and end position of this connection
+					// store start and end position of this
+					// connection
 					connections.push_back(GLuint(from - 1));
 					connections.push_back(GLuint(to - 1));
+					num_connections.push_back(n);
 				}
 				iteration++;
 			}
 		}
 
 		file.close();
-		std::cout << "read number of atoms: " << positions.size() << " connections: " << connections.size() / 2 << std::endl;
+		std::cout << "read number of atoms: " << positions.size()
+				  << " connections: " << connections.size() / 2 << std::endl;
 
 		return true;
 	}
 
-
 	/// initialize everything that needs the context
-	bool init(context& ctx)
-	{
+	bool init(context &ctx) {
 		// load data
-		if (file_name != "")
-			read_sdf_file(file_name);
+		if (file_name != "") read_sdf_file(file_name);
 
 		view_ptr = find_view_as_node();
 
 		// set background color to white
 		ctx.set_bg_clr_idx(4);
 
-		cgv::render::type_descriptor vec3type = cgv::render::element_descriptor_traits<cgv::vec3>::get_type_descriptor(positions[0]);
-		cgv::render::type_descriptor vec4type = cgv::render::element_descriptor_traits<cgv::vec4>::get_type_descriptor(colors[0]);
-		
+		cgv::render::type_descriptor vec3type =
+			cgv::render::element_descriptor_traits<
+				cgv::vec3>::get_type_descriptor(positions[0]);
+		cgv::render::type_descriptor vec4type =
+			cgv::render::element_descriptor_traits<
+				cgv::vec4>::get_type_descriptor(colors[0]);
+
 		// build shader program for spheres
 		if (!sphere_prog.build_program(ctx, "sphere_raycast.glpr")) {
 			std::cerr << "could not build sphere shader program" << std::endl;
@@ -204,32 +193,36 @@ public:
 
 		// - create buffer objects
 		bool success = true;
-		success = vb_pos.create(ctx, &(positions[0]), positions.size()) && success;
+		success =
+			vb_pos.create(ctx, &(positions[0]), positions.size()) && success;
 		success = vb_col.create(ctx, &(colors[0]), colors.size()) && success;
 		success = vertex_array_spheres.create(ctx) && success;
-		
+
+		glGenBuffers(1, &connection_ssbo);
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, connection_ssbo);
+		glBufferData(GL_SHADER_STORAGE_BUFFER,
+					 num_connections.size() * sizeof(int), &num_connections[0],
+					 GL_STATIC_DRAW);
+		glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+
 		// enable program to access attribute positions
 		sphere_prog.enable(ctx);
+		success =
+			vertex_array_spheres.set_attribute_array(
+				ctx, sphere_prog.get_position_index(), vec3type, vb_pos,
+				0,  // position is at start of the vector <-> offset = 0
+				positions.size(),  // number of position elements in the array
+				sizeof(cgv::vec3)  // stride from one element to next
+				) &&
+			success;
 		success = vertex_array_spheres.set_attribute_array(
-			ctx,
-			sphere_prog.get_position_index(),
-			vec3type,
-			vb_pos,
-			0,                                      // position is at start of the vector <-> offset = 0
-			positions.size(),                       // number of position elements in the array
-			sizeof(cgv::vec3) // stride from one element to next
-		) && success;
-		success = vertex_array_spheres.set_attribute_array(
-			ctx,
-			sphere_prog.get_color_index(),
-			vec4type,
-			vb_col,
-			0,                                      // position is at start of the vector <-> offset = 0
-			colors.size(),                          // number of color elements in the array
-			sizeof(cgv::vec4) // stride from one element to next
-		) && success;
+					  ctx, sphere_prog.get_color_index(), vec4type, vb_col,
+					  0,  // position is at start of the vector <-> offset = 0
+					  colors.size(),  // number of color elements in the array
+					  sizeof(cgv::vec4)  // stride from one element to next
+					  ) &&
+				  success;
 		sphere_prog.disable(ctx);
-
 
 		// build shader program for spheres
 		if (!cylinder_prog.build_program(ctx, "cylinder_raycast.glpr")) {
@@ -241,48 +234,44 @@ public:
 
 		cylinder_prog.enable(ctx);
 		success = vertex_array_cylinder.set_attribute_array(
-			ctx,
-			cylinder_prog.get_position_index(),
-			vec3type,
-			vb_pos,
-			0,                                          // position is at start of the vector <-> offset = 0
-			positions.size(),                         
-			sizeof(cgv::vec3) // stride from one element to next
-		) && success;
+					  ctx, cylinder_prog.get_position_index(), vec3type, vb_pos,
+					  0,  // position is at start of the vector <-> offset = 0
+					  positions.size(),
+					  sizeof(cgv::vec3)  // stride from one element to next
+					  ) &&
+				  success;
 		success = vertex_array_cylinder.set_attribute_array(
-			ctx,
-			cylinder_prog.get_color_index(),
-			vec4type,
-			vb_col,
-			0,                                      // position is at start of the vector <-> offset = 0
-			colors.size(),                          // number of color elements in the array
-			sizeof(cgv::vec4) // stride from one element to next
-		) && success;
+					  ctx, cylinder_prog.get_color_index(), vec4type, vb_col,
+					  0,  // position is at start of the vector <-> offset = 0
+					  colors.size(),  // number of color elements in the array
+					  sizeof(cgv::vec4)  // stride from one element to next
+					  ) &&
+				  success;
 		cylinder_prog.disable(ctx);
 		return true;
 	}
 
-
 	/// this method is called before the draw call of the current frame
-	void init_frame(context& ctx)
-	{
-		
-	}
-
+	void init_frame(context &ctx) {}
 
 	/// this method is called to draw a frame
-	void draw(context& ctx)
-	{
+	void draw(context &ctx) {
 		ctx.set_material(material_mol);
 		if (show_spheres) {
 			// Enable shader program we want to use for drawing
 			sphere_prog.enable(ctx);
 
-			float pixel_extent_per_depth = (float)(2.0*tan(0.5*0.0174532925199*view_ptr->get_y_view_angle()) / ctx.get_height());
-			sphere_prog.set_uniform(ctx, "pixel_extent_per_depth", pixel_extent_per_depth);
+			float pixel_extent_per_depth =
+				(float)(2.0 *
+						tan(0.5 * 0.0174532925199 *
+							view_ptr->get_y_view_angle()) /
+						ctx.get_height());
+			sphere_prog.set_uniform(ctx, "pixel_extent_per_depth",
+									pixel_extent_per_depth);
 			sphere_prog.set_uniform(ctx, "radius", radius);
 
-			// this is necessary for the compute_appearance functions to incorporate the passed color
+			// this is necessary for the compute_appearance functions to
+			// incorporate the passed color
 			sphere_prog.set_uniform(ctx, "map_color_to_material", 3);
 
 			vertex_array_spheres.enable(ctx);
@@ -293,16 +282,20 @@ public:
 			sphere_prog.disable(ctx);
 		}
 		if (show_cylinders) {
-
 			// Enable shader program we want to use for drawing
 			cylinder_prog.enable(ctx);
-			cylinder_prog.set_uniform(ctx, "radius", 0.5f*radius);
+			cylinder_prog.set_uniform(ctx, "radius", 0.5f * radius);
 
-			// this is necessary for the compute_appearance functions to incorporate the passed color
+			glBindBuffer(GL_SHADER_STORAGE_BUFFER, connection_ssbo);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, connection_ssbo);
+
+			// this is necessary for the compute_appearance functions to
+			// incorporate the passed color
 			cylinder_prog.set_uniform(ctx, "map_color_to_material", 3);
 
 			vertex_array_cylinder.enable(ctx);
-			glDrawElements(GL_LINES, (GLsizei)connections.size(), GL_UNSIGNED_INT, &connections.front());
+			glDrawElements(GL_LINES, (GLsizei)connections.size(),
+						   GL_UNSIGNED_INT, &connections.front());
 			vertex_array_cylinder.disable(ctx);
 
 			// Disable shader program and texture
@@ -313,9 +306,8 @@ public:
 			ctx.set_material(material_plane);
 			ctx.push_modelview_matrix();
 			ctx.mul_modelview_matrix(
-				cgv::math::scale4<double>(plane_size, plane_size, plane_size)*
-				cgv::math::translate4<double>(0,0,plane_offset)
-			);
+				cgv::math::scale4<double>(plane_size, plane_size, plane_size) *
+				cgv::math::translate4<double>(0, 0, plane_offset));
 			ctx.ref_surface_shader_program().enable(ctx);
 			ctx.tesselate_unit_square();
 			ctx.ref_surface_shader_program().disable(ctx);
@@ -324,58 +316,44 @@ public:
 		}
 	}
 
+	void destruct(context &ctx) {}
 
-	void destruct(context& ctx)
-	{
+	bool handle(cgv::gui::event &e) { return false; }
 
-	}
+	void stream_help(std::ostream &os) { os << "particle: ..." << std::endl; }
 
+	void stream_stats(std::ostream &os) {}
 
-	bool handle(cgv::gui::event& e)
-	{
-		return false;
-	}
-
-
-	void stream_help(std::ostream& os)
-	{
-		os << "particle: ..." << std::endl;
-	}
-
-
-	void stream_stats(std::ostream& os)
-	{
-
-	}
-
-
-	void create_gui()
-	{
+	void create_gui() {
 		add_decorator("particle", "heading");
 
 		bool parameter = true;
-		if (begin_tree_node("molecule", parameter , true)) {
+		if (begin_tree_node("molecule", parameter, true)) {
 			align("\a");
-			add_member_control(this, "sphere radius", radius, "value_slider", "min=0.1;max=1;ticks=true;log=true");
+			add_member_control(this, "sphere radius", radius, "value_slider",
+							   "min=0.1;max=1;ticks=true;log=true");
 			add_member_control(this, "show_spheres", show_spheres, "toggle");
-			add_member_control(this, "show_cylinders", show_cylinders, "toggle");
+			add_member_control(this, "show_cylinders", show_cylinders,
+							   "toggle");
 			align("\b");
 			end_tree_node(parameter);
 		}
-		if (begin_tree_node("plane", parameter , true)) {
+		if (begin_tree_node("plane", parameter, true)) {
 			align("\a");
 			add_member_control(this, "show_plane", show_plane, "toggle");
-			add_member_control(this, "plane_size", plane_size, "value_slider", "min=0.01;max=100;log=true;ticks=true");
-			add_member_control(this, "plane_offset", plane_offset, "value_slider", "min=-2;max=2;ticks=true");
+			add_member_control(this, "plane_size", plane_size, "value_slider",
+							   "min=0.01;max=100;log=true;ticks=true");
+			add_member_control(this, "plane_offset", plane_offset,
+							   "value_slider", "min=-2;max=2;ticks=true");
 			align("\b");
 			end_tree_node(parameter);
 		}
 		if (begin_tree_node("molecule material", material_mol, false)) {
-            align("\a");
-            add_gui("material", material_mol);
-            align("\b");
-            end_tree_node(material_mol);
-        }
+			align("\a");
+			add_gui("material", material_mol);
+			align("\b");
+			end_tree_node(material_mol);
+		}
 		if (begin_tree_node("plane material", material_plane, false)) {
 			align("\a");
 			add_gui("material", material_plane);
